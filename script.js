b02b5f9a3d489298bd75cbf27137ad303161f81f
Mahima3225 <mahimajain0531@gmail.com>
2025-04-19 22:11:32 +0530

    js added

---
+++ script.js
@@ -0,0 +1,45 @@
+// function for login page
+const validUsername = "user";
+const validPassword = "pass123";
+//for validation of login
+function validateLogin(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("uname");
+    const password = document.getElementById("psw");
+    const usernameError = document.getElementById("username-error");
+    const passwordError = document.getElementById("password-error");
+
+    let isValid = true;
+
+    // Reset error messages
+    usernameError.style.display = "none";
+    passwordError.style.display = "none";
+
+    if (username.value !== validUsername) {
+        usernameError.textContent = "Invalid Username";
+        usernameError.style.display = "block";
+        isValid = false;
+    }
+
+    if (password.value !== validPassword) {
+        passwordError.textContent = "Invalid Password";
+        passwordError.style.display = "block";
+        isValid = false;
+    }
+
+    if (isValid) {
+        window.location.href = "./pages/FEE(project).html";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
