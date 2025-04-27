c126f362f660286323f2d79e4232d41c5ba360cb
Mahima3225 <mahimajain0531@gmail.com>
2025-04-27 14:04:31 +0530

    sw

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function validateLogin(event) {//.
     passwordError.style.display = "none";//.
     //.
     //check if username is correct
-    if (username.value !== validUsername) {
+    if (username.value !== validUsername) {//.
         usernameError.textContent = "Invalid Username"; //.
         usernameError.style.display = "block";//.
         isValid = false;//.
